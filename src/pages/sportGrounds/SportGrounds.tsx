1fd65b8ca104a9b294c1dad7c18df4cd109ba34d
Igor Valnev <iavalnev@sberbank.ru>
2022-08-09 19:47:22 +0300

    Сделал предварительную разметку страницы

--- src/pages/sportGrounds/SportGrounds.tsx
+++ src/pages/sportGrounds/SportGrounds.tsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { Autocomplete, InputAdornment, TextField } from '@mui/material'
 import SportGroundsMap from './map/SportGroundsMap'
 import SportGroundList from './sportGroundList/SportGroundList'
 import classes from './sportGrounds.module.scss'
+// import SearchIcon from '@mui/icons-material/Search';
 
 const SportGrounds = () => {
   return (
     <section className={classes.sportGroundsContainer}>
-    <SportGroundsMap />
-    <SportGroundList />
+      <Autocomplete
+        options={[]}
+        renderInput={(params) => {
+          return (<div className={classes.searchWrapper}>
+            <TextField
+              {...params}
+              id='sportGroundSearch'
+              className={classes.searchInput}
+              placeholder='Поиск'
+              InputProps={{
+                ...params.InputProps,
+                type: 'search',
+                startAdornment: (
+                  <InputAdornment position="start">
+                    A
+                    {/* <SearchIcon /> */}
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>)
+        }}
+        noOptionsText={'Нет спортивных площадок'}
+      />
+      <div className={classes.mapContainer}>
+        <SportGroundsMap />
+        <SportGroundList />
+      </div>
     </section>
   )
 }
